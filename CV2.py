f4f1c4ea26ba8a0fca413d6855e561138fc9798e
J3igSmoke <96828105+J3igSmoke@users.noreply.github.com>
2022-08-31 11:48:16 +0200

    Add files via upload

---
+++ CV2.py
@@ -0,0 +1,29 @@
+import time
+
+import PIL.Image
+import cv2
+import numpy
+import numpy as np
+import pyautogui
+
+
+slika = pyautogui.screenshot()
+slika.save('zajeta slika1.jpg')
+img = cv2.imread("zajeta slika1.jpg");
+gimg = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+template = cv2.imread("mario.PNG", cv2.IMREAD_GRAYSCALE);
+
+w, h = template.shape[::-1]
+
+
+result = cv2.matchTemplate(gimg, template, cv2.TM_CCOEFF_NORMED)
+
+
+loc = np.where(result >= 0.78) #Natačnost
+
+for pt in zip(*loc[::-1]):
+    cv2.rectangle(img, pt, (pt[0] + w, pt[1] + h), (50,255,20), 1)
+
+cv2.imshow('res.png', img)
+
+cv2.waitKey(0)
